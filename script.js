95e9916a326b431a59bb74f779eabae29fed3d9a
Abiduzzaman Abid <itisabidss@gmail.com>
2018-05-21 03:33:34 +0000

    Task 4

--- script.js
+++ script.js
@@ -13,6 +13,8 @@ function sumArray (arr){
 
 console.log(sumArray([2,3,4,5,6]));
 
+console.log("Task 3: Check An Email:");
+
 function checkEmail(emailString) {
     var emailFormat =/^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/
    var match = emailFormat.test(emailString);
@@ -21,3 +23,29 @@ function checkEmail(emailString) {
 
 console.log(checkEmail("itisabidss@gmail.com"));
 console.log(checkEmail("Shamahaque@gmail.com"));
+
+console.log("Task 4: Objects:");
+
+var library = [ 
+   {
+       author: 'Bill Gates',
+       title: 'The Road Ahead',
+       readingStatus: true
+   },
+   {
+       author: 'Steve Jobs',
+       title: 'Walter Isaacson',
+       readingStatus: true
+   },
+   {
+       author: 'Suzanne Collins',
+       title:  'Mockingjay: The Final Book of The Hunger Games', 
+       readingStatus: false
+   }];
+
+
+function getReadingStatus (index) {
+    return library[index].readingStatus;
+}
+console.log("The reading status is:");
+console.log(getReadingStatus(1));
