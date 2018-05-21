ca798853f346d9071941124b3b7ba55b1d50eeaa
Abiduzzaman Abid <itisabidss@gmail.com>
2018-05-21 03:04:53 +0000

    Task 2

--- script.js
+++ script.js
@@ -1,2 +1,14 @@
 /* JavaScript code here */
-console.log("external Js")
+console.log("external Js");
+
+console.log("Task 2: Sum of an Array:");
+
+function sumArray (arr){
+    var sum=0;
+    arr.forEach(function(value,index){
+        sum += value;
+    });
+    return sum;
+}
+
+console.log(sumArray([2,3,4,5,6]));
